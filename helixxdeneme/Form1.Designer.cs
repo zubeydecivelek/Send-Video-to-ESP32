14750a0676035c67b76797949b24977ebe052cbd
zubeydecivelek <zubeydeecivelek@gmail.com>
2022-10-09 19:35:43 +0300

    ....

---
+++ helixxdeneme/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace helixxdeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.sendData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 147);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "dosya seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(461, 115);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(244, 127);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(405, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(61, 104);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(146, 128);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "BAĞLAN";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // sendData
+            // 
+            this.sendData.Location = new System.Drawing.Point(61, 273);
+            this.sendData.Name = "sendData";
+            this.sendData.Size = new System.Drawing.Size(172, 76);
+            this.sendData.TabIndex = 4;
+            this.sendData.Text = "GÖNDER";
+            this.sendData.UseVisualStyleBackColor = true;
+            this.sendData.Click += new System.EventHandler(this.sendData_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 48);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "bağlantıyı kes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(312, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "veri al";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(429, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sendData);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button sendData;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ helixxdeneme/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms.Integration;
+using System.IO;
+using System.Net.Sockets;
+
+namespace helixxdeneme
+{
+    public partial class Form1 : Form
+    {
+
+        OpenFileDialog file;
+        string videoPath;
+        string videoName;
+        byte[] bytes;
+
+        string temp = Environment.CurrentDirectory;
+
+        TcpClient tcpClient = new TcpClient();
+        NetworkStream networkStream = null;
+        public StreamWriter clientData;
+        private StreamWriter streamWriter;
+        private StreamReader streamReader;
+
+        public NetworkStream NetworkStream { get => networkStream; set => networkStream = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            file = ofd;
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                videoPath = file.FileName;
+                videoName = file.SafeFileName;
+                MessageBox.Show("Gönderilmek istenen dosya:" + videoPath, "", MessageBoxButtons.OKCancel);
+                bytes = File.ReadAllBytes(videoPath);
+              /*  String writed = "";
+                for (long i = 0; i < bytes.Length; i++)
+                {
+                    writed += bytes[i] + "\n"; 
+                }*/
+                textBox.Text = bytes.Length.ToString();
+
+               
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = temp + @"\Saves\" + "video" + DateTime.Now.ToString("dd.MM.yyyy") + ".avi";
+
+            
+            using (FileStream fs = File.Create("video" + DateTime.Now.ToString("dd.MM.yyyy") + ".avi"))
+            {
+                textBox.Text = videoName;
+                // Add some information to the file.
+                fs.Write(bytes, 0, bytes.Length);
+            }
+
+            // Open the stream and read it back.
+            /*using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }*/
+        }
+
+        public void CreateConnection()
+        {
+            tcpClient.Connect("192.168.4.1", 80);
+            NetworkStream = tcpClient.GetStream();
+            streamWriter = new StreamWriter(NetworkStream);
+            if (tcpClient.Connected)
+            {
+                textBox1.Text = "BAĞLANDI";
+            }
+
+        }
+        public void Disconnect()
+        {
+            tcpClient.Dispose();
+            tcpClient.Close();
+            NetworkStream.Close();
+            if (!tcpClient.Connected)
+            {
+                textBox1.Text = "BAĞLANTI KESİLDİ";
+            }
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            CreateConnection();
+        }
+
+        private void sendData_Click(object sender, EventArgs e)
+        {
+            SendData();
+        }
+
+        public void SendData()
+        {
+            if (NetworkStream.CanWrite)
+            {
+                NetworkStream.Write(bytes, 0, bytes.Length);
+                textBox1.Text = "GÖNDERİLİYOR";
+                /*byte[] senddata = {-128};
+                NetworkStream.Write([-128],0,1);*/
+                NetworkStream.Flush();
+               
+            }
+            else if (!NetworkStream.CanWrite)
+            {
+                textBox1.Text="You can not read data from this stream";
+                
+            }
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NetworkStream = tcpClient.GetStream();
+            streamReader = new StreamReader(NetworkStream);
+            int komut = -1;
+            while ((komut = streamReader.Read()) != -1) 
+            {
+                char c = (char)komut;
+                if (c == 'C')
+                {
+                    textBox2.Text = "GÖNDERİLDİ";
+                    break;
+                }
+
+            }
+        }
+    }
+}
